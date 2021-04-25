0279fd94bed8599b79bbfa3a3f4899b8674851b4
YEHP <67673893+Yohan-H@users.noreply.github.com>
2021-04-25 10:42:48 -0500

    prubas

---
+++ Registro/src/ControlEdu.java
@@ -0,0 +1,16 @@
+
+public class ControlEdu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] prueba = new int[5];
+		 
+		prueba[0]=85;
+		prueba[2]=20;
+		prueba[4]=10;
+		
+		System.out.println ("prueba" +prueba[4]);
+
+	}
+
+}
